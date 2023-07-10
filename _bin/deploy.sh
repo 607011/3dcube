b3ddee487f8fb0cfc1e156d20b66df39073fab29
Oliver Lau <oliver@ersatzworld.net>
2023-07-10 12:56:51 +0200

    1st working version.

---
+++ _bin/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source .env
+
+mkdir -p deploy
+
+# rsync -rav --delete src/static deploy
+
+for HTMLFILE in cube.html
+do
+    html-minifier \
+        --collapse-whitespace \
+        --remove-comments \
+        --remove-optional-tags \
+        --remove-redundant-attributes \
+        --remove-script-type-attributes \
+        --remove-tag-whitespace \
+        --minify-css true \
+        --minify-js true \
+        <src/$HTMLFILE >deploy/$HTMLFILE
+done
+
+for JSFILE in cube.js
+do
+    uglifyjs --compress --mangle < src/$JSFILE > deploy/$JSFILE
+done
+
+rsync -rav --delete deploy/* $REMOTE
